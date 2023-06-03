5fe254642c7549035bd2851f6c59f8b879d4422c
ChihYuLin <chihyulin66@gmail.com>
2023-06-03 10:13:04 +0800

    deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
+composer install 
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
@@ -11,3 +12,6 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+
+
